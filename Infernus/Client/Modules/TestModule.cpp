590fb4e90ec680bdc22609dcf608948cc5351d1b
EchoHackCmd <EchoHackCmd@gmail.com>
2020-12-27 13:38:41 +0000

    MultiPlayerLevel stuff

--- Infernus/Client/Modules/TestModule.cpp
+++ Infernus/Client/Modules/TestModule.cpp
@@ -3,15 +3,10 @@
 void TestModule::onGmTick() {
 	LocalPlayer* Player = Minecraft::ClientInstance()->LocalPlayer();
 	if (Player != nullptr) {
-		MultiPlayerLevel* curr = Player->MultiPlayerLevel;
-		size_t size = curr->getListSize();
-		if (size > 0 && size <= 5000) {
-			for (size_t I = 0; I < size; I++) {
-				Actor* currPlayer = curr->get(I);
-				if (currPlayer != nullptr && currPlayer != Player) {
-					currPlayer->setNameTagVisible(true);
-				}
-			}
+		Actor* facingEnt = Player->MultiPlayerLevel->facingActor;
+		if (facingEnt && Minecraft::ClientInstance()->MinecraftGame()->canUseKeys()) {
+			Minecraft::GameMode()->interact(facingEnt, facingEnt->getPos());
+			Player->swing();
 		}
 	}
 }
--- Infernus/SDK/Classes/MultiPlayerLevel.h
+++ Infernus/SDK/Classes/MultiPlayerLevel.h
@@ -5,7 +5,12 @@ public:
 	char pad_0008[72]; //0x0008
 	uint64_t listStart; //0x0050
 	uint64_t listEnd; //0x0058
-	char pad_0060[32]; //0x0060
+	char pad_0060[2264]; //0x0060
+	uint32_t facingFlag; //0x0938
+	uint32_t blockFace; //0x093C
+	Vec3_i facingBlockPos; //0x0940
+	char pad_094C[12]; //0x094C
+	class Actor* facingActor; //0x0958
 
 	/* VTable Functions (Not currently defined) */
 
