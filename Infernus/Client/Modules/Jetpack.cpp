b5bf6d1c41948b25987c225c7033dc55add8b48c
EchoHackCmd <EchoHackCmd@gmail.com>
2020-12-29 09:02:06 +0000

    Update Jetpack.cpp

--- Infernus/Client/Modules/Jetpack.cpp
+++ Infernus/Client/Modules/Jetpack.cpp
@@ -21,6 +21,11 @@ void Jetpack::onTick() {
 	if (Player != nullptr) {
 		Vec2 currRot = Player->bodyRot;
 		Vec2 angles = Vec2((currRot.x) * -(PI / 180.f), (currRot.y + 90.0f) * (PI / 180.f));
-		Player->velocity = Vec3(cos(angles.y) * cos(angles.x) * speed, sin(angles.x) * speed, sin(angles.y) * cos(angles.x) * speed);
+		if (this->State == "Bypass") {
+			//R3coil you know what to do..
+		}
+		else {
+			Player->velocity = Vec3(cos(angles.y) * cos(angles.x) * speed, sin(angles.x) * speed, sin(angles.y) * cos(angles.x) * speed);
+		}
 	}
 }
