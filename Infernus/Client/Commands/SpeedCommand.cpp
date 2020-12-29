9e5f36102126797dd9c981341f2f1a74c465cde9
EchoHackCmd <EchoHackCmd@gmail.com>
2020-12-29 08:25:53 +0000

    Typo

--- Infernus/Client/Commands/SpeedCommand.cpp
+++ Infernus/Client/Commands/SpeedCommand.cpp
@@ -10,7 +10,7 @@ void SpeedCommand::execute(std::string input, std::vector<std::string> words) {
 				this->respond("Set speed to " + words.at(1));
 			}
 			else {
-				this->respond("Something went wrong fetching the Speed Command!");
+				this->respond("Something went wrong fetching the Speed Module!");
 			}
 		}
 		else {
