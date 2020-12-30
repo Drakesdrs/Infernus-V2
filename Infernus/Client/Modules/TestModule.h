c7852223171023e9d05cf55d7c5eb9c412bdbc98
Hackerboi69420 <Hackerboi69420@users.noreply.github.com>
2020-12-30 23:47:04 +0100

    fly

--- Infernus/Client/Modules/TestModule.h
+++ Infernus/Client/Modules/TestModule.h
@@ -3,6 +3,12 @@
 
 class TestModule : public Module {
 public:
-	TestModule() : Module::Module("Test Module", "Other", "Testing") {};
+	TestModule() : Module::Module("Test Module", "Other", "Testing", 0x46) {};
 	void onGmTick();
+	//void onLerp(Actor* nigga, Vec3 niggaknocc);
+	void onDisable() { speed = 5; timer = 0; if (Minecraft::ClientInstance()->LocalPlayer() != nullptr) Minecraft::ClientInstance()->LocalPlayer()->velocity = Vec3(0, 0, 0); State = "Fast "; };
+private:
+	int timer = 0;
+	float speed = 5;
+	Vec3 leapbegin;
 };
