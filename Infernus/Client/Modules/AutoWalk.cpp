3ced1be5d1812fd113b6aecf9dfdd0b5b0f14dde
EchoHackCmd <EchoHackCmd@gmail.com>
2020-12-31 17:57:35 +0000

    Update AutoWalk.cpp

--- Infernus/Client/Modules/AutoWalk.cpp
+++ Infernus/Client/Modules/AutoWalk.cpp
@@ -2,14 +2,10 @@
 
 void AutoWalk::onTick() {
 
-	if (Minecraft::ClientInstance() != 0) {
-		if (Minecraft::ClientInstance()->LocalPlayer() != 0) {
-			LocalPlayer* Player = Minecraft::ClientInstance()->LocalPlayer();
-
-			Player->velocity.z = sin((Minecraft::ClientInstance()->LocalPlayer()->bodyRot.y + 90) * (3.14159 / 180.0f)) * 0.235f;
-			Player->velocity.x = cos((Minecraft::ClientInstance()->LocalPlayer()->bodyRot.y + 90) * (3.14159 / 180.0f)) * 0.235f;
-
-		}
+	if (Player != nullptr) {
+		float yaw = Player->bodyRot.y;
+		Player->velocity.x = sin((yaw + 90.f) * (3.14159 / 180.0f)) * 0.235f;
+		Player->velocity.z = cos((yaw + 90.f) * (3.14159 / 180.0f)) * 0.235f;
 	}
 }
 
