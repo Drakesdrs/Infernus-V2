01d7df82a1faabb616bf7615a3d1a177bad92a68
EchoHackCmd <EchoHackCmd@gmail.com>
2020-12-31 17:42:52 +0000

    Update MineplexDisabler.h

--- Infernus/Client/Modules/MineplexDisabler.h
+++ Infernus/Client/Modules/MineplexDisabler.h
@@ -3,7 +3,7 @@
 
 class MineplexDisabler : public Module {
 public:
-	MineplexDisabler() : Module::Module("MemeplexDisablerxd", "Exploits", "XD") {};
+	MineplexDisabler() : Module::Module("MemeplexDisablerxd", "Exploits", "Bypass Mineplex's AntiCheat") {};
 	void onPacket(PacketType type, void* Packet, bool* cancel);
 private:
 	int counter = 0;
