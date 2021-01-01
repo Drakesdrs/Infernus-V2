3ca81d5fa237da08b5fcd23db2ff9a412cf46ad1
Hackerboi69420 <Hackerboi69420@users.noreply.github.com>
2021-01-01 18:45:49 +0100

    lol

--- Infernus/Client/Modules/MineplexDisabler.h
+++ Infernus/Client/Modules/MineplexDisabler.h
@@ -3,7 +3,7 @@
 
 class MineplexDisabler : public Module {
 public:
-	MineplexDisabler() : Module::Module("MemeplexDisablerxd", "Exploits", "Bypass Mineplex's AntiCheat") {};
+	MineplexDisabler() : Module::Module("MemeplexDisabler", "Exploits", "Bypass Mineplex's AntiCheat") {};
 	void onPacket(PacketType type, void* Packet, bool* cancel);
 private:
 	int counter = 0;
